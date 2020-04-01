151c6ab73ccda452d685b3b45d34be70f2cf995f
Grandi-Bambino <dmitry.ryabo@gmail.com>
2020-04-01 16:44:12 +0300

    получение репозитория с сети

--- HomeWork/app/src/main/java/ru/geekbrains/poplib/mvp/model/repo/GithubUsersRepo.kt
+++ HomeWork/app/src/main/java/ru/geekbrains/poplib/mvp/model/repo/GithubUsersRepo.kt
@@ -1,4 +1,8 @@
 package ru.geekbrains.poplib.mvp.model.repo
 
-class GithubUsersRepo {
+import io.reactivex.rxjava3.schedulers.Schedulers
+import ru.geekbrains.poplib.mvp.model.api.IDataSource
+
+class GithubUsersRepo(val api: IDataSource) {
+    fun getUser(userName: String) = api.getUser(userName).subscribeOn(Schedulers.io())
 }
