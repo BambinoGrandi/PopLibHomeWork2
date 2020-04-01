72f76ac4c265265f40377a5a3df84214d0219d46
Grandi-Bambino <dmitry.ryabo@gmail.com>
2020-04-01 16:44:35 +0300

    подключение библиотеки для рабо с изображениями

--- HomeWork/app/src/main/java/ru/geekbrains/poplib/ui/image/GlideImageLoader.kt
+++ HomeWork/app/src/main/java/ru/geekbrains/poplib/ui/image/GlideImageLoader.kt
@@ -1,4 +1,14 @@
 package ru.geekbrains.poplib.ui.image
 
-class GlideImageLoader {
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import ru.geekbrains.poplib.mvp.model.image.IImageLoader
+
+class GlideImageLoader : IImageLoader<ImageView>{
+    override fun loadImage(url: String, container: ImageView) {
+        Glide.with(container.context)
+            .load(url)
+            .into(container)
+    }
+
 }
