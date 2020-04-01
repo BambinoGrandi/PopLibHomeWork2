6903b5af47f5e72f16a431cefd7db09d9690069b
Grandi-Bambino <dmitry.ryabo@gmail.com>
2020-04-01 16:44:55 +0300

    интерфейс для работы с retrofit

--- HomeWork/app/src/main/java/ru/geekbrains/poplib/mvp/model/api/IDataSource.kt
+++ HomeWork/app/src/main/java/ru/geekbrains/poplib/mvp/model/api/IDataSource.kt
@@ -1,4 +1,16 @@
 package ru.geekbrains.poplib.mvp.model.api
 
+import io.reactivex.rxjava3.core.Single
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Url
+import ru.geekbrains.poplib.mvp.model.entity.GithubRepository
+import ru.geekbrains.poplib.mvp.model.entity.GithubUser
+
 interface IDataSource {
+    @GET("/users/{user}")
+    fun getUser(@Path("user") userName: String): Single<GithubUser>
+
+    @GET
+    fun getRepos(@Url url: String) : Single<List<GithubRepository>>
 }
